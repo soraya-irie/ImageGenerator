91f0bb0a43126ef511494747b297bf7dd606b425
soraya-ire <rujiangkongshi@gmail.com>
2026-04-27 17:07:57 +0900

    Initial Commit

---
+++ ImageGenerator/ImageGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageGeneratorApp.swift
+//  ImageGenerator
+//
+//  Created by soraya irie on 2026/04/27.
+//
+
+import SwiftUI
+
+@main
+struct ImageGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
