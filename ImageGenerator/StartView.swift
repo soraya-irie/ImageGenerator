9bdcef6b2b2b23b5fd414f1b31e79bb671293b57
soraya-ire <rujiangkongshi@gmail.com>
2026-04-28 00:07:12 +0900

    Complete Section2 Step2 - utilize image generation features

--- ImageGenerator/StartView.swift
+++ ImageGenerator/StartView.swift
@@ -2,7 +2,11 @@ import SwiftUI
 
 struct StartView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 16) {
+            Text("Create a Unique Dish")
+                .font(.largeTitle.weight(.semibold))
+        }
+        .padding()
     }
 }
 
