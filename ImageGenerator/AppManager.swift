ea397192b85ff467a5dcf4923b79137c36124c91
soraya-ire <rujiangkongshi@gmail.com>
2026-04-28 18:19:09 +0900

    Complete Section3 Step8 - utilize image generation features

--- ImageGenerator/AppManager.swift
+++ ImageGenerator/AppManager.swift
@@ -5,15 +5,19 @@ class AppManager {
     let imageGenerator = ImageGenerator()
 
     private(set) var error: Error?
+    private(set) var isGenerating = false
 
     func generateImage() {
         error = nil
+        isGenerating = true
 
         Task {
             do {
                 let generatedImage = try await imageGenerator.generate()
+                isGenerating = false
             } catch {
                 self.error = error
+                isGenerating = false
             }
         }
     }
