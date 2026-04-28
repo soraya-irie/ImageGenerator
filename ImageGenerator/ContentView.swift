1f8bcebaeb0354aebce144d0c3ded8dd0808fa4b
soraya-ire <rujiangkongshi@gmail.com>
2026-04-28 18:33:10 +0900

    Complete Section3 Step11 - utilize image generation features

--- ImageGenerator/ContentView.swift
+++ ImageGenerator/ContentView.swift
@@ -1,13 +1,8 @@
-//
-//  ContentView.swift
-//  ImageGenerator
-//
-//  Created by soraya irie on 2026/04/27.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(AppManager.self) private var appManager
+
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -21,4 +16,5 @@ struct ContentView: View {
 
 #Preview {
     ContentView()
+        .previewEnvironment()
 }
