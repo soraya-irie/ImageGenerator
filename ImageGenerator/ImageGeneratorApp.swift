81c7e817d751e16d9928854face70604b718183e
soraya-ire <rujiangkongshi@gmail.com>
2026-04-28 18:28:40 +0900

    Complete Section3 Step10 - utilize image generation features

--- ImageGenerator/ImageGeneratorApp.swift
+++ ImageGenerator/ImageGeneratorApp.swift
@@ -2,9 +2,12 @@ import SwiftUI
 
 @main
 struct ImageGeneratorApp: App {
+    @State var appManager = AppManager()
+
     var body: some Scene {
         Window("ImageGenerator", id: "main") {
             ContentView()
+                .environment(appManager)
         }
     }
 }
