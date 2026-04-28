bd1d9e4e3f8b0a1facfeed8e810bae9a21123309
soraya-ire <rujiangkongshi@gmail.com>
2026-04-28 18:12:04 +0900

    Complete Section3 Step6 - utilize image generation features

--- ImageGenerator/AppManager.swift
+++ ImageGenerator/AppManager.swift
@@ -3,4 +3,14 @@ import Foundation
 @Observable
 class AppManager {
     let imageGenerator = ImageGenerator()
+
+    func generateImage() {
+        Task {
+            do {
+                let generatedImage = try await imageGenerator.generate()
+            } catch {
+
+            }
+        }
+    }
 }
