fc705decd53b44bd61c5bec22361bc808b8963c8
soraya-ire <rujiangkongshi@gmail.com>
2026-04-29 14:15:27 +0900

    Complete Section7 Step8 - utilize image generation features

--- ImageGenerator/IngredientListView.swift
+++ ImageGenerator/IngredientListView.swift
@@ -2,9 +2,13 @@ import SwiftUI
 
 struct IngredientListView: View {
     @Environment(AppManager.self) private var appManager
+    @State private var newIngredient = ""
 
     var body: some View {
         VStack(alignment: .leading) {
+            TextField("Add ingredients (optional)", text: $newIngredient)
+                .textFieldStyle(.roundedBorder)
+
             Text("Added Ingredients")
                 .font(.body.bold())
                 .padding(.vertical, 8)
