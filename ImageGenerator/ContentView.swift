e689c7458c1dc571cf50c4d04ff6f94c07e9511c
soraya-ire <rujiangkongshi@gmail.com>
2026-04-29 15:45:11 +0900

    Complete Section9 Step5 - utilize image generation features

--- ImageGenerator/ContentView.swift
+++ ImageGenerator/ContentView.swift
@@ -11,6 +11,7 @@ struct ContentView: View {
                 StartView()
             }
         }
+        .frame(minWidth: ImageGenerator.imageSize, maxWidth: 400, minHeight: 550)
         .overlay {
             if appManager.isGenerating {
                 loadingView()
